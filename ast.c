ec4fa6dee2de2d01175ae18a28f684636d045f2e
Karan Kadam <karank@outlook.com>
2018-08-25 19:54:52 +0930

    Added choice menu

--- ast.c
+++ ast.c
@@ -10,5 +10,34 @@ typedef struct ast_node_t
 
 int main(int argc, char** argv)
 {
+
+	printf("Simple implementation of an AST in C\n");
+	int choice = 3; // Exit
+
+	while(1)
+	{
+		printf("============================\n");
+		printf("Please choose an operation -\n");
+		printf("1. Add node\n");
+		printf("2. Print all nodes\n");
+		printf("3. Exit\n");
+		printf("============================\n\n");
+
+		scanf("%d", &choice);
+
+		switch(choice)
+		{
+			case 1:
+				printf("Add Node\n");
+				break;
+			case 2:
+				printf("Print All Nodes\n");
+				break;
+			default:
+				printf("Invalid choice.\n");
+			case 3:
+				return 0;
+		}
+	}
 	return 0;
 }
