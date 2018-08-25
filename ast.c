bf5e8c61f135373dcf0e1c37b89d7621ca0adbc4
Karan Kadam <karank@outlook.com>
2018-08-26 00:25:28 +0930

    Added add node and print nodes. Not working yet

--- ast.c
+++ ast.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct ast_node_t
 {
@@ -8,11 +9,62 @@ typedef struct ast_node_t
 	struct ast_node_t** children;	
 } ast_node_t;
 
+ast_node_t* ROOT;
+
+void add_node(ast_node_t* parent, char* tag, char* content)
+{
+	ast_node_t* new_node = (ast_node_t *)malloc(sizeof(ast_node_t));
+	new_node->tag = tag;
+	new_node->content = content;
+	new_node->child_count = 0;
+
+	*parent->children = new_node;
+	parent->child_count++;
+	parent->children++;
+}
+
+void print_nodes(ast_node_t* node)
+{
+	if(node)
+	{
+		printf("tag: %s\n", node->tag);
+		printf("content: %s\n", node->content);
+		printf("child_count: %s\n", node->child_count);
+
+		// Print children of this child node
+		while(*node->children++)
+			print_nodes(*node->children);
+	}
+}
+
+void delete_nodes(ast_node_t* root)
+{
+	ast_node_t* node = *root->children;
+
+	if(node && node->children)
+	{
+		// Print children of this child node
+		delete_nodes(*node->children++);
+
+		// Next child
+		root->children++;
+	}
+
+	if(node)
+		free(node);
+}
+
 int main(int argc, char** argv)
 {
+	// Create a root node
+	ROOT = (ast_node_t *)malloc(sizeof(ast_node_t));
+	if(!ROOT)
+		printf("Critical error - Failed to allocate space for root node. Exiting.\n");
 
 	printf("Simple implementation of an AST in C\n");
 	int choice = 3; // Exit
+	char tag[10];
+	char content[10];
 
 	while(1)
 	{
@@ -28,10 +80,14 @@ int main(int argc, char** argv)
 		switch(choice)
 		{
 			case 1:
-				printf("Add Node\n");
+				printf("Enter tag:");
+				scanf("%s",&tag);
+				printf("Enter content:");
+				scanf("%s",&content);
+				add_node(ROOT, tag, content);
 				break;
 			case 2:
-				printf("Print All Nodes\n");
+				print_nodes(ROOT);
 				break;
 			default:
 				printf("Invalid choice.\n");
